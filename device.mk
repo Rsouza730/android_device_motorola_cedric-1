fe8453d95f169333d6b2a1c9837eab1013f512a5
Alberto97 <albertop2197@gmail.com>
2017-09-26 17:54:16 +0200

    cedric: Add seccomp policy

--- device.mk
+++ device.mk
@@ -267,6 +267,10 @@ PRODUCT_PACKAGES += \
     libxml2 \
     libshim_ril
 
+# Seccomp
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
