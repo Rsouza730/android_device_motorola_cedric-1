bae7e91f936986c34d37937285751ed7202f5252
Wzedlare <vedatak01@gmail.com>
2017-11-19 17:09:52 +0000

    cedric: Disable init for now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,8 +129,8 @@ USE_OPENGL_RENDERER := true
 BOARD_HAVE_QCOM_FM := true
 
 # Init
-TARGET_INIT_VENDOR_LIB := libinit_cedric
-TARGET_RECOVERY_DEVICE_MODULES := libinit_cedric
+#TARGET_INIT_VENDOR_LIB := libinit_cedric
+#TARGET_RECOVERY_DEVICE_MODULES := libinit_cedric
 
 # Keymaster
 TARGET_PROVIDES_KEYMASTER := true
