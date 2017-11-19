3e37c29749222c989eb51f2c81ecc38a30bdbfab
Luca Stefani <luca020400@lineageos.org>
2017-11-19 16:07:46 +0000

    cedric: Import HIDL HALs

--- device.mk
+++ device.mk
@@ -54,6 +54,9 @@ PRODUCT_AAPT_CONFIG := normal
 
 # Audio
 PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.broadcastradio@1.0-impl \
     audiod \
     audio.a2dp.default \
     audio.primary.msm8937 \
@@ -97,10 +100,12 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl \
     libbt-vendor
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
     camera.msm8937 \
     libbson \
     libshim_camera \
@@ -118,6 +123,12 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.renderscript@1.0-impl \
     gralloc.msm8937 \
     copybit.msm8937 \
     liboverlay \
@@ -127,6 +138,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
     libprotobuf-cpp-lite
 
 # Ebtables
@@ -137,16 +149,24 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    fingerprintd
+    android.hardware.biometrics.fingerprint@2.1-service
 
 # Fingerprint IDC
 PRODUCT_COPY_FILES += \
     device/motorola/cedric/idc/uinput-fpc.idc:system/usr/idc/uinput-fpc.idc
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl
+
 # FM
 PRODUCT_PACKAGES += \
     FMRadio \
     libfmjni
+
+# GPS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl
 
 # GPS Configurations
 PRODUCT_COPY_FILES += \
@@ -183,8 +203,13 @@ PRODUCT_COPY_FILES += \
     device/motorola/cedric/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl \
     device/motorola/cedric/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl
 
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl
+
 # Lights
 PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
     lights.msm8937
 
 # Media
@@ -215,6 +240,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power@1.0-impl \
     power.msm8937
 
 # Ramdisk
@@ -242,17 +268,33 @@ PRODUCT_PACKAGES += \
     libxml2 \
     libshim_ril
 
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl
+
 # Sensor Configurations
 PRODUCT_COPY_FILES += \
     device/motorola/cedric/configs/sensors/hals.conf:system/etc/sensors/hals.conf \
     device/motorola/cedric/configs/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf
 
 # Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl
+
 PRODUCT_COPY_FILES += \
     device/motorola/cedric/configs/thermal-engine-cedric.conf:system/etc/thermal-engine-cedric.conf
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
+# Vibrator HAL
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl
+
 # Wifi
 PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service \
     wifi_symlinks \
     hostapd_default.conf \
     hostapd \
